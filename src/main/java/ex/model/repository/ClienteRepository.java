20f21b56be82f37a6dc33db7483637d221b837b0
Victor Jose <victorjosevjq543@gmail.com>
2025-03-16 20:57:27 -0300

    first commit

---
+++ src/main/java/ex/model/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package ex.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ex.model.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+
+}
+
